687f792ed328e3895368edde173f15b59b3b7e3b
Fengjiao Gu <gufengjiao@tuxingkeji.com>
2024-08-13 12:30:17 +0800

    * 准备完善 model 部分

--- app/model.py
+++ app/model.py
@@ -5,3 +5,9 @@ model_router = APIRouter(prefix="/model", tags=["model"])
 @model_router.get("/")
 async def read_users():
     return [{"username": "Rick"}, {"username": "Morty"}]
+
+
+
+# TODO: 和 svn 进行联动，本地要存储一个缓存,没有缓存的话直接从 svn 上进行下载
+
+
