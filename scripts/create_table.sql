1c1d6f26fe14e3468ba2ca7d5f166538382a247f
jokker_50 <18761609908@163.com>
2024-09-03 20:32:03 +0800

    * 晚上回去,保存一下

--- scripts/create_table.sql
+++ scripts/create_table.sql
@@ -1,12 +1,6 @@
 
 
-
-CREATE DATABASE SaturnTest CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- USE Saturn_Database_V1;
 USE SaturnTest;
-
--- DROP TABLE UcdJsonTag;
 
 CREATE TABLE UcdJsonTag (
     tag CHAR(200) NOT NULL,
