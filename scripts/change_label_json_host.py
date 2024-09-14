ababf1753634bec1cc65c165b1171b2ef444e132
jokker_50 <18761609908@163.com>
2024-09-14 17:31:39 +0800

    * 将 file 复制到 label 本地

--- scripts/change_label_json_host.py
+++ scripts/change_label_json_host.py
@@ -1,9 +1,13 @@
 
+import os
+import shutil
 from JoTools.utils.FileOperationUtil import FileOperationUtil
 from JoTools.utils.JsonUtil import JsonUtil
 
 
-label_dir = r"/home/ldq/Data/labels"
+label_dir       = r"/home/ldq/Data/labels"
+label_image_dir       = r"/home/ldq/Data/labels/images"
+customer_dir    = r"/home/ldq/Data/customer_file"
 
 for each in FileOperationUtil.re_all_file(label_dir, endswitch=[".json"]):
     json_info = JsonUtil.load_data_from_json_file(each)
@@ -11,11 +15,18 @@ for each in FileOperationUtil.re_all_file(label_dir, endswitch=[".json"]):
     for obj in json_info["pic_describe"]:
         print(obj[1])
         obj[1] = obj[1].replace("192.168.3.50", "ENV_HOST")
+        # obj[1] = obj[1].replace("", "ENV_HOST")
 
-    JsonUtil.save_data_to_json_file(json_info, each)
 
+        file_name = os.path.split(obj[1])[1]
 
-# copy to label image dir
+        customer_file_path = os.path.join(customer_dir, file_name)
+        image_file_path = os.path.join(label_image_dir, file_name)
+
+        if not os.path.exists(image_file_path):
+            shutil.copy(customer_file_path, image_file_path)
+
+    # JsonUtil.save_data_to_json_file(json_info, each)
 
 
 
